89100ed31f770c791a7072aa1a22e2cb673b219d
MisterNo0ne <84687450+MisterNo0ne@users.noreply.github.com>
2022-09-28 23:39:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 for (let i=0; i<5; i++) {
   console.log("<p" + i + "> TEST!! </p" + i + ">");
-  document.body.innerHTML = "<p" + i + "> TEST!! </p" + i + ">";
+  document.body.innerHTML = "<h1>hi :)</h1>";
 }
 document.body.innerHTML = "<p>Done</p>";
-
+console.log("AMONG US!!!");
