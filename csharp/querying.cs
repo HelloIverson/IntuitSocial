55b0ecab0d8fe8b4fb753d5a680a1afe3e7a69dc
HelloIverson <102616794+HelloIverson@users.noreply.github.com>
2022-09-29 18:55:07 -0600

    cs :eyes:

---
+++ csharp/querying.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Data;
+using System.Data.SqlClientl;
+namespace querying {
+class Main {
+
+    public static void main(string[] args) {
+        Console.Writeline("AMONG US!!!!!!");
+    }
+}
+}
