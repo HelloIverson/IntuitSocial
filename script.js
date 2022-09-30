f35ad3bbc1251e669b7811e2c5f1eb6634babb9c
MisterNo0ne <84687450+MisterNo0ne@users.noreply.github.com>
2022-09-29 21:38:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+console.log("Start");
 function setup() {
-  line(15, 25, 70, 90);
+  createCanvas(400, 400);
+}
+console.log("Setup complete");
+function draw() {
+  background(220);
+  noStroke();
+  fill(255, 0, 0);
+  ellipse(120, 60, 60, 30);
+  rect(90, 60, 60, 100);
+  fill(220);
+  rect(110, 120, 20, 40);
+  strokeWeight(2);
+  stroke(0);
+  fill(255);
+  ellipse(100, 75, 50, 25);
 }
 console.log("Done!");
