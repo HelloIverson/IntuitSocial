1e222d4bb01c9c9dc33dda810864662af518dacc
HelloIverson <102616794+HelloIverson@users.noreply.github.com>
2022-10-08 19:56:57 -0600

    Bug fixes

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-@@ -9,5 +9,5 @@ function draw() {
-}
-
 function regFuc() {
   window.location.href = "IntuitSocial/pages/register.html";
   window.location.href = "/IntuitSocial/pages/register.html";
