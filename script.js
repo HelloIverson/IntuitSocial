2c239cfb1a7d94b3ee042543dbcf821fe47a6e27
HelloIverson <102616794+HelloIverson@users.noreply.github.com>
2022-10-09 23:22:43 -0600

    All buttons working

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function regFuc() {
 function promoFuc() {
   window.location.href = "https://docs.google.com/document/d/1h0Vkw4Do8C6rqaU6jM-JahmT-Vb5rPXOuCFlPOHFcZ8/edit?usp=sharing";
 }
+
+function legalFuc() {
+  window.location.href = "https://docs.google.com/document/d/1GeIpqoMdMegVLhqE9uM_9NYFAqFwtaersYdH01c_uyM/edit?usp=sharing";
+}
+
+function cashflowFuc() {
+  window.location.href = "https://docs.google.com/document/d/1irOGRtx40zWYylcDKzLr-PPFB1xVlFPeHBFzu15RVvo/edit?usp=sharing";
+}
+
+function quickbooksFuc() {
+  window.location.href = "https://docs.google.com/document/d/10yQH4aBfFnM2bZtegeGGrQevu2IZ8p2NNCrH7Iwsiw8/edit?usp=sharing";
+}
