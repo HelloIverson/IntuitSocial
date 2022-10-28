bfd91a1726aec98c4502d708589cf3a598fb6448
HelloIverson <102616794+HelloIverson@users.noreply.github.com>
2022-10-27 22:06:03 -0600

    Standardization

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <!doctype html>
+<html lang="en-US">
 <html>
    <head>
+      <meta charset="UTF-8">
       <title>PJSK Home</title>
       <script src="https://cdn.jsdelivr.net/npm/p5@1.4.2/lib/p5.js"></script>
       <link
