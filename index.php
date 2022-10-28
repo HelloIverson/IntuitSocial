f2790b3d2526d975859b14f39fa6b71654268260
HelloIverson <102616794+HelloIverson@users.noreply.github.com>
2022-10-27 22:14:38 -0600

    Does this launch?

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <!doctype html>
-<?php
-require_once('header.php');
-?>
-
+<html lang="en-US">
+<html>
+   <head>
+      <meta charset="UTF-8">
+      <title>PJSK Home</title>
+      <script src="https://cdn.jsdelivr.net/npm/p5@1.4.2/lib/p5.js"></script>
+      <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+    />
+      <link rel="stylesheet" href="index.css">
+      <script src="script.js"></script>
+   </head>
+   <body> 
       <div class="toppage">
          <div class="title">
          <h1>PJSK:</h1>
