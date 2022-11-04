41519dd848a92956f7fbbafab8f9b910eb53e9c8
HelloIverson <102616794+HelloIverson@users.noreply.github.com>
2022-11-03 22:18:50 -0600

    php changes

---
+++ index.php
@@ -0,0 +1,109 @@
+<?php
+require('header.php');
+?>
+
+<div class="topnav">
+         <a href="index.html"><img class="logo" src="images/pjsklol.jpg" alt="logo"></a>
+         <a class="nav-link" href="pages/about.html">Resources</a>
+         <a class="nav-link" href="pages/register.html">Register</a>
+         <input type="text" placeholder="Search..">
+      </div>
+      
+      <div class="toppage">
+         <div class="title">
+         <h1>PJSK:</h1>
+         <div class="statlist">
+            <h2 class="importance">Prepare. Join. Support. Konnect.</h2>
+            <div class="stat">
+               <h3 class="number">49%+</h3>
+               <p>Of loans going to white business owners.</p>
+            </div>
+         
+            <div class="stat">
+               <h2 class="number">134k+</h2>
+               <p>Black-owned employer businesses in the US.</p>
+            </div>
+
+            <div class="stat">
+               <h2 class="number">1.4m+</h2>
+               <p>LGBTQ business owners in the US.</p>
+            </div>
+         </div>
+         </div>
+      </div>
+
+
+      <div class="mapbox">
+         <div class="map">
+            <iframe src="https://www.google.com/maps/d/embed?mid=1ev76YpyMxwNVz1nepyl4tm_81AlAo-U&ehbc=2E312F" width="640" height="480">People that we have helped!</iframe>
+         </div>
+
+         <div class="community">
+            <h2 class="importance">Find your PJSK <br>Community today!</h2>
+            <p>Businesses and aspiring business 
+               <br>owners across the nation can find 
+               <br>the PJSK network near them. 
+               <br>Finding a PJSK community in your 
+               <br>local area can help spark the 
+               <br>creation of tomorrow's business!
+            </p>
+         </div>
+
+         <button onclick="regFuc()">
+            Register now!
+         </button>
+      </div>
+
+      <div class="why">
+         <h2 class="importance">Why Us?</h2>
+         <div class="why-us-txt">
+            <p>So, what is the PJSK method? Well, our power is in Konnection. We provide
+               the platform and the resources that you need to not only find people near
+               you, but to find people like you. People who have the experiences that you've
+               had, the knowledge that you may not. Our power is in our people and that means
+               our power is in you! Everything we do is based off of what our communities can do,
+               and everything you do gives back to your communities. So get Konnected with your PJSK
+               Kommunity today!
+            </p>
+         </div>
+         <div class="why-us-img">
+            <img src="images/why_us.jpg">
+         </div>
+      </div>
+
+      <div  class="testimoniallist">
+         <div class="testimonial">
+            <h2 class="importance">"Building courage, confidence, and optimism: I like the concept! 
+               I believe that something like this would be very helpful, 
+               especially if its something useful to people starting off." 
+            </h2>
+
+            <h2 class="ihatethis">- David Neville, LGBTQ+ Entrepreneur</h2>
+
+            <img src="images/testimonial_ichi.jpg" alt="david_neville">
+         </div>
+            
+         <div class="testimonial">
+            <h2 class="importance">
+               "PJSK has tremendous potential to help people enter the business world.
+                I wish I had access to a resource like this when I entered the real-estate world. "
+            </h2>
+
+            <h2 class="ihatethis">
+               - Anhkim Le, Vietnamese Entrepreneur
+            </h2>
+
+            <img src="images/anhkim_le.jpg" alt="anhkim_le">
+         </div>
+
+         <div class="testimonial">
+            <h2 class="importance">
+               “I think PJSK is one of the most innovative ways 
+               I’ve seen anyone address this issue. Using communities 
+               to build resources and relationships- ingenious!”</h2>
+
+            <h2 class="ihatethis">- Karina Sucahyo, Indonesian Entrepreneur</h2>
+         </div>
+      </div>
+   </body>
+</html>
